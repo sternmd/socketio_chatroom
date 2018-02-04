247469518d83c3524fd222d5ebedf91adcc19dff
Max Stern <stern.maxwell@gmail.com>
2018-02-04 15:01:44 -0600

    Emit chat events

--- index.js
+++ index.js
@@ -15,4 +15,10 @@ var io = socket(server);
 
 io.on('connection', function(socket) {
   console.log('made socket connection', socket.id);
+
+  // Listen for chat Message
+  socket.on('chat', function(data){
+    io.sockets.emit('chat', data);
+    console.log(data);
+  })
 })
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,23 @@
 // Make connection
 var socket = io.connect("http://localhost:3000");
+
+
+// Query DOM
+var message = document.getElementById('message'),
+    handle  = document.getElementById('handle'),
+    output  = document.getElementById('output'),
+    button  = document.getElementById('send');
+
+// Emit events
+button.addEventListener('click', function() {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+})
+
+// Listen for events
+socket.on('chat', function(data){
+    output.innerHTML += "<p><strong>" + data.handle + ": </strong> " + data.message+ "</p>";
+
+});
