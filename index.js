365b980037e9efde0de6f4578ed662db120f0105
Max Stern <stern.maxwell@gmail.com>
2018-02-04 17:30:37 -0600

    add clientCount, style changes, ES6.

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-var express = require('express');
-var socket = require('socket.io');
-var app = express();
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
 
 // Server
-var server = app.listen(3000, () => {
+let server = app.listen(3000, () => {
   console.log('listening to request on port 3000');
 })
 
 // Static files
 app.use(express.static('public'));
 
-// Socket setup
-var io = socket(server);
+// Socket connection / setup
+let io = socket(server);
 
-io.on('connection', function(socket) {
-  console.log('made socket connection', socket.id);
+io.on('connection', socket => {
+  let clientCount = io.engine.clientsCount;
+  console.log('made socket connection:', socket.id);
 
   // Listen for chat Message
-  socket.on('chat', function(data){
+  socket.on('chat', data => {
     io.sockets.emit('chat', data);
     console.log(data);
-  })
+  });
 
   // Listen for typing
-  socket.on('typing', function(data){
+  socket.on('typing', data => {
     socket.broadcast.emit('typing', data);
-  })
-})
+  });
+
+  // User count
+  io.sockets.emit("user count", clientCount);
+  socket.on("disconnect", () => {
+    io.sockets.emit("user count", clientCount);
+  });
+
+});
--- public/chat.js
+++ public/chat.js
@@ -1,32 +1,37 @@
 // Make connection
-var socket = io.connect("http://localhost:3000");
+let socket = io.connect("http://localhost:3000");
 
 
 // Query DOM
-var message   = document.getElementById('message'),
+let message   = document.getElementById('message'),
     handle    = document.getElementById('handle'),
     output    = document.getElementById('output'),
     feedback  = document.getElementById('feedback'),
+    users     = document.getElementById('users'),
     button    = document.getElementById('send');
 
 // Emit events
-button.addEventListener('click', function() {
+button.addEventListener('click', () => {
     socket.emit('chat', {
         message: message.value,
         handle: handle.value
     });
 });
 
-message.addEventListener('keypress', function() {
+message.addEventListener('keypress', () => {
     socket.emit('typing', handle.value);
 });
 
 // Listen for events
-socket.on('chat', function(data){
+socket.on('chat', data => {
     feedback.innerHTML = "";
-    output.innerHTML += "<p><strong>" + data.handle + ": </strong> " + data.message+ "</p>";
+    output.innerHTML += `<p><strong>${data.handle}:</strong> ${data.message}</p>`;
 });
 
-socket.on('typing', function(data){
-    feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
+socket.on('typing', data => {
+    feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
 });
+
+socket.on('user count', count => {
+    users.innerHTML = count;
+})
